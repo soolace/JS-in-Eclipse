aaeb81cd33060bcf59d4fd9a74793b13686dc896
soolace <mscheuzger@netlive.ch>
2018-08-17 14:00:30 +0200

    more Fields

--- javascript.js
+++ javascript.js
@@ -6,42 +6,58 @@ function checkForm() {
 	var emailInputField = document.getElementById('i_mail');
 	var email = emailInputField.value;
 	if (email.length < 4) {
-		setError(emailInputField, "Email too short");
+		setError(emailInputField, "email too short");
 	} else if (email.search('@') < 1) {
-		setError(emailInputField, "invalid Email");
+		setError(emailInputField, "invalid email");
 	} else {
 		clearError(emailInputField);
 	}
-	
+
 	var textInputField = document.getElementById('i_name');
 	var name = textInputField.value;
-	if (name.length <2){
+	if (name.length < 2) {
 		setError(textInputField, "name too short");
-	} else{
+	} else {
+		clearError(textInputField);
+	}
+
+	var textInputField = document.getElementById('i_name2');
+	var name2 = textInputField.value;
+	if (name2.length < 2) {
+		setError(textInputField, "familyname too short");
+	} else {
 		clearError(textInputField);
 	}
 	
-	var textInputField = document.getElementById('i_name2');
-	var name2 = textInputField.value;
-	if (name2.length <2){
-		setError(textInputField, "name too short");
-	} else{
+	var telInputField = document.getElementById('i_tel');
+	var tel = telInputField.value;
+	if (tel.length < 10) {
+		setError(telInputField, "please enter your phone number");
+	} else {
+		clearError(telInputField);
+	}
+	
+	var textInputField = document.getElementById('i_bday');
+	var bday = textInputField.value;
+	if (bday.length < 10) {
+		setError(textInputField, "please enter your birthday");
+	} else {
 		clearError(textInputField);
 	}
-
-
+	
 }
 
 // setError function
 function setError(element, message) {
 	var x = document.getElementbyId
 	element.parentElement.className = "has-error";
-	element.title= message;
-	
+	element.title = message;
+
 }
 
 // clearError function
 function clearError(element) {
-	element.parentElement.className -= "has-error";	
-	element.title= "";
+	element.parentElement.className -= "has-error";
+	element.title = "";
 }
+
